1f3abfc84b3a62b358012ac8554446c8e5f86a0a
SoulaimaneCherkaoui <soulaimanecherkaoui0@gmail.com>
2024-12-18 09:49:50 +0100

    first commit

---
+++ src/main/java/ma/enset/soulaimane/KafkaGrafanaApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.soulaimane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaGrafanaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaGrafanaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/enset/soulaimane/KafkaGrafanaApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.soulaimane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaGrafanaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
